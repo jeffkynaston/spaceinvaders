e4564dcfb881f973e529e28f4eb94e4a34e535e9
Kendall Carey & Steven Miyakawa "SAM <pair+kendallcarey.samsamskies@devbootcamp.com>
2014-07-19 19:20:54 -0700

    add dbc logo as player

--- player.js
+++ player.js
@@ -2,17 +2,16 @@ function Player() {
   this.x = 400,
   this.y = 700,
   this.width = 80,
-  this.height = 40,
+  this.height = 80,
   this.alive = true
 };
 
 Player.prototype = {
 
   drawPlayer: function(context, myPlayer) {
-    context.beginPath();
-    context.rect(myPlayer.x, myPlayer.y, myPlayer.width, myPlayer.height);
-    context.fillStyle = 'blue';
-    context.fill();
+    var img = $('#dbc-logo')[0];
+
+    context.drawImage(img, myPlayer.x, myPlayer.y , myPlayer.width, myPlayer.height);
   },
 
   moveRight: function() {
